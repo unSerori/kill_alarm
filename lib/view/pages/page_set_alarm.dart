892a7cb8f4040db27922f7ef21249273c2959001
yadu2222 <ochaocha2004@gmail.com>
2023-08-25 12:01:38 +0900

    うまくいかないよ～～～～～

--- lib/view/pages/page_set_alarm.dart
+++ lib/view/pages/page_set_alarm.dart
@@ -1,4 +1,4 @@
-import 'dart:ffi';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
@@ -53,13 +53,14 @@ class _boxAlarm extends State<boxAlarm> {
             mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              Text("うごく？"),
+              //Text('${call_hour_data}:${call_min_data}'),
               //設定時刻表示
-              // CustomText(
-              //   text: '${call_hour_data}:${call_min_data}', //文字列の結合には${}を使えばいいらしい　そうなの～～～！？！？！？
-              //   fontSize: 60,
-              //   Color: Constant.black,
-              // ),
+              CustomText(
+                text:
+                    '${call_hour_data}:${call_min_data}', //文字列の結合には${}を使えばいいらしい　そうなの～～～！？！？！？
+                fontSize: 60,
+                Color: Constant.black,
+              ),
 
               //スイッチ
               CupertinoSwitch(
@@ -68,7 +69,6 @@ class _boxAlarm extends State<boxAlarm> {
                   onChanged: (value) {
                     setState(() {
                       _bool = value;
-                      //timerData["timers"].add("ひどい");
                     });
                   }),
             ],
@@ -84,7 +84,6 @@ class _boxAlarm extends State<boxAlarm> {
                   size: 40,
                 ),
                 onPressed: () {
-                  //別ページ遷移
                   //ページ遷移
                   Navigator.push(
                     context,
@@ -103,8 +102,82 @@ class _boxAlarm extends State<boxAlarm> {
   }
 }
 
+/*
+//アラーム部分
+class AlarmList extends StatefulWidget {
+  const AlarmList({Key? key}) : super(key: key);
+  @override
+  State<AlarmList> createState() => _AlarmList();
+}
+
+class _AlarmList extends State<AlarmList> {
+  //時刻 hour
+  static List<String> call_hour = ["00"];
+  //時刻 min
+  static List<String> call_min = ["00"];
+  //起こし方
+  static List<List<String>> call_type = [
+    ["音", "水", "光", "叩く", "振動"]
+  ];
+
+  static bool _bool = true;
+  static Widget _AlarmListVew() {
+    return ListView.builder(
+        itemCount: _boxAlarm.timerData.length,
+        itemBuilder: (context, index) {
+          return Card(
+            color: Constant.white,
+            elevation: 0,
+            child: SizedBox(
+                height: 70,
+                child: Column(
+                  children: [
+                    Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          CustomText(
+                              text: call_hour[index],
+                              fontSize: 40,
+                              Color: Constant.black),
+                          const CustomText(
+                              text: ':', fontSize: 40, Color: Constant.black),
+                          CustomText(
+                              text: call_min[index],
+                              fontSize: 40,
+                              Color: Constant.black),
+                          // //スイッチ
+                          // CupertinoSwitch(
+                          //     activeColor: Constant.accent_color,
+                          //     value: _bool,
+                          //     onChanged: (value) {
+                          //       setState(() {
+                          //         _bool = value;
+                          //       });
+                          //     }),
+                        ]),
+                    Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Container(
+                            width: 30,
+                            color: Constant.main,
+                            child: Text(call_type[index][0]),
+                          ),
+                        ]),
+                  ],
+                )),
+          );
+        });
+  }
+}
+*/
+
 //ボタン表示
 //動的な動作を実装するためのなんかあれ
+//失敗してる
+/*
 class MyStatefulWidget extends StatefulWidget {
   @override
   State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
@@ -114,25 +187,21 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   bool isButtonPressed = false;
   static String word = '';
 
-  
   @override
   Widget build(BuildContext context) {
     //メソッドをインスタンスから呼び出す
     return _BottunState("音", widget);
-    
   }
 
   Widget _BottunState(String word, Widget widget,
       {Color color = Constant.grey, Color subcolor = Constant.sub_color}) {
     return SizedBox(
-      width: 100,
+      width: 40,
       height: 40,
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
-          primary: isButtonPressed ? Constant.sub_color : Constant.main,
-          shape: const StadiumBorder(),
-        ),
-
+            primary: isButtonPressed ? Constant.sub_color : Constant.main,
+            alignment: Alignment.center),
         //ボタンを押したときの動作
         onPressed: () {
           widget;
@@ -140,20 +209,17 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
             isButtonPressed = !isButtonPressed;
           });
         },
-
         //中に入る文字
-        child: Text(word),
-        // child: CustomText(
-        //   text: word,
-        //   fontSize: 40,
-        //   Color: Constant.black,
-        // ),
+        child: CustomText(
+          text: word,
+          fontSize: 16,
+          Color: Constant.black,
+        ),
       ),
     );
   }
-
-  
 }
+*/
 
 class PageSetAlarm extends StatefulWidget {
   const PageSetAlarm({Key? key}) : super(key: key);
@@ -163,6 +229,16 @@ class PageSetAlarm extends StatefulWidget {
 }
 
 class _PageSetAlarmState extends State<PageSetAlarm> {
+// 初期のウィジェットリスト
+  List<Widget> list = <Widget>[
+    _boxAlarm(),
+    
+  ];
+
+  //表示するウィジェットリスト
+  List<Widget> _items = <Widget>[];
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -212,11 +288,18 @@ class PageSetAlarm_2 extends StatefulWidget {
 
 class _PageSetAlarmState_2 extends State<PageSetAlarm_2> {
   //ボタン管理用の変数
-  static const bool voice = false;
-  static const bool water = false;
-  static const bool shine = false;
-  static const bool panch = false;
-  static const bool vib = false;
+  static bool voice = false;
+  static bool water = false;
+  static bool shine = false;
+  static bool panch = false;
+  static bool vib = false;
+
+  //ボタン色変更用の変数(敗北者)
+  static Color color_voice = Constant.sub_color;
+  static Color color_water = Constant.sub_color;
+  static Color color_shine = Constant.sub_color;
+  static Color color_panch = Constant.sub_color;
+  static Color color_vib = Constant.sub_color;
 
   @override
   Widget build(BuildContext context) {
@@ -234,8 +317,8 @@ class _PageSetAlarmState_2 extends State<PageSetAlarm_2> {
           child: Column(
             children: [
               Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                // mainAxisSize: MainAxisSize.max,
+                // mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   //左側の余白
                   const Padding(
@@ -300,11 +383,125 @@ class _PageSetAlarmState_2 extends State<PageSetAlarm_2> {
               //箱2
               Components.whiteBox(
                 Column(children: [
+                  const Text('起こし方を選んでね'),
+
+                  //ボタンたち
                   Row(
-                    mainAxisSize: MainAxisSize.max,
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      MyStatefulWidget(),
+                      //MyStatefulWidget(),
+                      //音
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: color_voice,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          elevation: 3,
+                        ),
+                        onPressed: () {
+                          voice = !voice;
+                          setState(() {
+                            if (voice) {
+                              color_voice = Constant.main;
+                            } else {
+                              color_voice = Constant.sub_color;
+                            }
+                          });
+                        },
+                        child: const CustomText(
+                            text: '音', fontSize: 18, Color: Constant.black),
+                      ),
+                      Spacer(),
+
+                      //水
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: color_water,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          elevation: 3,
+                        ),
+                        onPressed: () {
+                          water = !water;
+                          setState(() {
+                            if (water) {
+                              color_water = Constant.main;
+                            } else {
+                              color_water = Constant.sub_color;
+                            }
+                          });
+                        },
+                        child: const CustomText(
+                            text: '水', fontSize: 18, Color: Constant.black),
+                      ),
+                      Spacer(),
+
+                      //光
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: color_shine,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          elevation: 3,
+                        ),
+                        onPressed: () {
+                          shine = !shine;
+                          setState(() {
+                            if (shine) {
+                              color_shine = Constant.main;
+                            } else {
+                              color_shine = Constant.sub_color;
+                            }
+                          });
+                        },
+                        child: const CustomText(
+                            text: '光', fontSize: 18, Color: Constant.black),
+                      ),
+                      Spacer(),
+
+                      //叩く
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: color_panch,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          elevation: 3,
+                        ),
+                        onPressed: () {
+                          panch = !panch;
+                          setState(() {
+                            if (panch) {
+                              color_panch = Constant.main;
+                            } else {
+                              color_panch = Constant.sub_color;
+                            }
+                          });
+                        },
+                        child: const CustomText(
+                            text: '叩く', fontSize: 18, Color: Constant.black),
+                      ),
+                      Spacer(),
+
+                      //振動
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: color_vib,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10)),
+                          elevation: 3,
+                        ),
+                        onPressed: () {
+                          vib = !vib;
+                          setState(() {
+                            if (vib) {
+                              color_vib = Constant.main;
+                            } else {
+                              color_vib = Constant.sub_color;
+                            }
+                          });
+                        },
+                        child: const CustomText(
+                            text: '振動', fontSize: 18, Color: Constant.black),
+                      ),
                     ],
                   )
                 ]),
@@ -323,7 +520,20 @@ class _PageSetAlarmState_2 extends State<PageSetAlarm_2> {
                     primary: Constant.accent_color,
                     shape: const StadiumBorder(),
                   ),
-                  onPressed: () {},
+                  onPressed: () {
+                    // _boxAlarm.timerData.addAll(
+                    //   {
+                    //     "call_hour": _boxAlarm.call_hour_data,
+                    //     "call_min" :_boxAlarm.call_min_data,
+                    //     "payloads":[
+                    //       {
+                    //         "name":""
+                    //       }
+                    //     ]
+                    //   }
+                    // );
+                    Navigator.pop(context); // 前のページに戻る
+                  },
                   child: Text('保存'),
                   //const CustomText(
                   //   text: '保存',
@@ -344,7 +554,28 @@ class _PageSetAlarmState_2 extends State<PageSetAlarm_2> {
                     primary: Constant.grey,
                     shape: const StadiumBorder(),
                   ),
-                  onPressed: () {},
+                  onPressed: () {
+                    //ダイアログ仮置き 書き換え予定
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return CupertinoAlertDialog(
+                          title: Text("本当に削除しても\nいいですか"),
+                          actions: <Widget>[
+                            CupertinoDialogAction(
+                              child: Text("はい"),
+                              isDestructiveAction: true,
+                              onPressed: () => Navigator.pop(context),
+                            ),
+                            CupertinoDialogAction(
+                              child: Text("いいえ"),
+                              onPressed: () => Navigator.pop(context),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
                   child: Text('削除'),
                 ),
               ),
